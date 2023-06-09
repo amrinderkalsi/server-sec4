ce2e524fc9df6857e9f3c1c80a1701e0087f5fd5
Ammy Kalsi <amrinder.kalsi13.com>
2023-06-09 12:07:00 -0400

    Created express server and added get and post request for issues data

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const issues = [
+    {
+      id: 1, 
+      status: 'Open', 
+      owner: 'Ravan',
+      created: new Date('2016-08-15'), 
+      effort: 5, 
+      completionDate: undefined,
+      title: 'Error in console when clicking Add',
+    },
+    {
+      id: 2, 
+      status: 'Assigned', 
+      owner: 'Eddie',
+      created: new Date('2016-08-16'), 
+      effort: 14, 
+      completionDate: new Date('2016-08-30'),
+      title: 'Missing bottom border on panel',
+    },
+  ];
+
+app.get('/api/issues', (req, res) => {
+    const metadata = {"totalCount" : issues.length};
+    res.json({
+        "metaData": metadata, "records": issues
+    });
+})
+
+app.post('/api/issues', (req, res) => {
+    console.log(req.body);
+    const newIssue = req.body;
+    newIssue.id = issues.length + 1;
+    newIssue.status = 'New';
+    newIssue.created = new Date();
+    issues.push(newIssue);
+    console.log('res.json', newIssue)
+    res.json(newIssue);
+})
+
+
+app.listen(5002, () => {
+    console.log('Server started on port 5002');
+});
